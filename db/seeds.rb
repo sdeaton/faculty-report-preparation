fb99c84d2fb7e0b6fc22eac62b1e97dab7fd6557
Brent Walther <brentwalther@gmail.com>
2016-03-28 14:25:14 -0500

    Create Evaluation model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+prng = Random.new
+
+(1..3).each do |i|
+  Evaluation.create(
+    term: '2015C',
+    subject: 'CSCE',
+    course: '110',
+    section: (500 + i).to_s,
+    instructor: 'Tiffani Williams',
+    enrollment: prng.rand(20..50),
+    item1_mean: prng.rand(3.0..5.0),
+    item2_mean: prng.rand(3.0..5.0),
+    item3_mean: prng.rand(3.0..5.0),
+    item4_mean: prng.rand(3.0..5.0),
+    item5_mean: prng.rand(3.0..5.0),
+    item6_mean: prng.rand(3.0..5.0),
+    item7_mean: prng.rand(3.0..5.0),
+    item8_mean: prng.rand(3.0..5.0)
+  )
+end
+
+(1..3).each do |i|
+  Evaluation.create(
+    term: '2015C',
+    subject: 'CSCE',
+    course: '111',
+    section: (500 + i).to_s,
+    instructor: 'Joseph Daniel Hurley',
+    enrollment: prng.rand(20..50),
+    item1_mean: prng.rand(3.0..5.0),
+    item2_mean: prng.rand(3.0..5.0),
+    item3_mean: prng.rand(3.0..5.0),
+    item4_mean: prng.rand(3.0..5.0),
+    item5_mean: prng.rand(3.0..5.0),
+    item6_mean: prng.rand(3.0..5.0),
+    item7_mean: prng.rand(3.0..5.0),
+    item8_mean: prng.rand(3.0..5.0)
+  )
+end
+
+(1..8).each do |i|
+  Evaluation.create(
+    term: '2015C',
+    subject: 'CSCE',
+    course: '121',
+    section: (500 + i).to_s,
+    instructor: 'Walter Daugherity',
+    enrollment: prng.rand(20..50),
+    item1_mean: prng.rand(3.0..5.0),
+    item2_mean: prng.rand(3.0..5.0),
+    item3_mean: prng.rand(3.0..5.0),
+    item4_mean: prng.rand(3.0..5.0),
+    item5_mean: prng.rand(3.0..5.0),
+    item6_mean: prng.rand(3.0..5.0),
+    item7_mean: prng.rand(3.0..5.0),
+    item8_mean: prng.rand(3.0..5.0)
+  )
+end
