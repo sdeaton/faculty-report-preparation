eb6e46960732e770840aa81c370ab8ddf30f66a7
Brent Walther <brentwalther@gmail.com>
2016-03-29 13:22:03 -0500

    Add faculty course table with dummy formulas.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,18 @@
 class Instructor < ActiveRecord::Base
   has_many :evaluations
+
+  def courses_taught
+    eval_columns = (1..8).map { |num| "AVG(item#{num}_mean)" }.join("+")
+    grouped_courses.select([
+      :term,
+      :subject,
+      :course,
+      "SUM(enrollment) as total_enrollment",
+      "((#{eval_columns}) / 8.0) as mean_eval_score"
+    ])
+  end
+
+  def grouped_courses
+    evaluations.group([:term, :subject, :course])
+  end
 end
