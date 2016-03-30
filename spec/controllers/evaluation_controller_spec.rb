411bc5dd887282e2f3d0e660a8fe638e9c8a077a
sdeaton <scott.deaton@gmail.com>
2016-03-30 21:27:45 +0000

    Added a few tests for evaluation controller

--- spec/controllers/evaluation_controller_spec.rb
+++ spec/controllers/evaluation_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe EvaluationController, type: :controller do
-
+  describe "GET #evaluation" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+    it "renders the evaluation index" do
+      get :index
+      expect(response).to render_template("evaluation/index")
+    end
+    it "assigns @evaluations" do
+        eval = Evaluation.create
+        get :index
+        expect(assigns(:evaluations)).to eq([eval])
+    end
+  end
 end
