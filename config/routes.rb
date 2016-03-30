760ace4d0233a86de3abf135d8020074747763bd
Henry Qiu <henryqiu315@gmail.com>
2016-03-30 21:06:02 +0000

    expanded cucumber tests, removed pills from front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,6 +13,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+  
+  root :to => 'home#index'
+  
   resources :evaluation do
     get 'export', on: :collection
   end
@@ -19,6 +23,7 @@ Rails.application.routes.draw do
   resources :instructor do
     get 'export', on: :member
   end
+  
 
   # Example resource route with options:
   #   resources :products do
