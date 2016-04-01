8ee6273cb2d102fa0ba81c3b8a2f551ab0513da2
Brent Walther <brentwalther@gmail.com>
2016-04-01 17:14:09 -0500

    Actually create new evaluations when importing.

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -31,8 +31,37 @@ class EvaluationController < ApplicationController
     redirect_to evaluation_index_path
   end
 
+  # TODO: clean this up a little but. It should be easy to follow, but it's a little long.
   def upload
     importer = ::PicaReportImporter.new(params.require(:data_file))
-    importer.evaluation_hashes
+    creation_results = importer.evaluation_hashes.map do |eval_attrs|
+      key_attrs, other_attrs = split_attributes(eval_attrs)
+
+      evaluation = Evaluation.where(key_attrs).first_or_initialize
+      is_new_record = evaluation.new_record?
+      evaluation.save
+
+      evaluation.update(other_attrs)
+
+      is_new_record
+    end
+
+    num_new_records = creation_results.count { |result| result == true }
+    num_updated_records = creation_results.length - num_new_records
+
+    flash[:notice] = "#{num_new_records} new evaluations imported. #{num_updated_records} evaluations updated."
+    redirect_to evaluation_index_path
+  end
+
+  private
+  def split_attributes(all_attrs)
+      # key attributes are ones for which we should have one unique record for a set of them
+      key_attributes = all_attrs.select { |k,v| [:term, :subject, :course, :section].include?(k) }
+
+      # other atttributes are ones that should either be assigned or updated
+      other_attributes = all_attrs.select { |k,v| ![:term, :subject, :course, :section].include?(k) }
+      other_attributes[:instructor] = Instructor.where(name: other_attributes[:instructor]).first_or_create
+
+      [ key_attributes, other_attributes ]
   end
 end
