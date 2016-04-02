e634f583703dd18dfcafb29173e0c506e11e9298
Brent Walther <brentwalther@gmail.com>
2016-04-02 12:23:29 -0500

    Add more evaluation model specs.

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -12,6 +12,12 @@ class Evaluation < ActiveRecord::Base
   end
 
   def self.default_sorted_groups
+    # We group by the following things and then sort the groups in this order:
+    #  - Term (2015C, 2015A, 2014C)
+    #  - Subject (CSCE, ENGR)
+    #  - Course (110, 111, 121)
+    #  - Instructor (Williams, Hurley)
+    #  - First character of section (200s, 500s are grouped together)s
     all.group_by do |eval|
       eval.term.to_s + eval.subject.to_s + eval.course.to_s + eval.instructor.id.to_s + eval.section.to_s[0]
     end.sort { |group1, group2| group1.first <=> group2.first }.map(&:last)
--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe Evaluation, type: :model do
   end
 
   describe "#create_if_needed_and_update" do
-
     let (:key_attrs) { {:term=>"2015C", :subject=>"CSCE", :course=>110, :section=>501} }
     let (:other_attrs) { {:instructor=> instructor , :enrollment=>24, :item1_mean=>4.46,
       :item2_mean=>4.46, :item3_mean=>4.46, :item4_mean=>4.08, :item5_mean=>4.46,
@@ -39,6 +38,54 @@ RSpec.describe Evaluation, type: :model do
 
       Evaluation.create_if_needed_and_update(key_attrs, other_attrs)
       expect(Evaluation.all.count).to eq(1)
+    end
+  end
+
+  describe "#default_sorted_groups" do
+    it "groups terms together" do
+      g11 = Evaluation.create term: "2014C", subject: "CSCE", course: "110", section: "501", instructor: instructor
+      g12 = Evaluation.create term: "2014C", subject: "CSCE", course: "110", section: "502", instructor: instructor
+      g21 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "501", instructor: instructor
+      g22 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "502", instructor: instructor
+
+      expect(Evaluation.default_sorted_groups).to eq([[g11, g12], [g21, g22]])
+    end
+
+    it "groups subject together" do
+      g11 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "501", instructor: instructor
+      g12 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "502", instructor: instructor
+      g21 = Evaluation.create term: "2015C", subject: "ENGR", course: "110", section: "501", instructor: instructor
+      g22 = Evaluation.create term: "2015C", subject: "ENGR", course: "110", section: "502", instructor: instructor
+
+      expect(Evaluation.default_sorted_groups).to eq([[g11, g12], [g21, g22]])
+    end
+
+    it "groups courses together" do
+      g11 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "501", instructor: instructor
+      g12 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "502", instructor: instructor
+      g21 = Evaluation.create term: "2015C", subject: "CSCE", course: "111", section: "501", instructor: instructor
+      g22 = Evaluation.create term: "2015C", subject: "CSCE", course: "111", section: "502", instructor: instructor
+
+      expect(Evaluation.default_sorted_groups).to eq([[g11, g12], [g21, g22]])
+    end
+
+    it "groups 200s and 500s sections together" do
+      g11 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "201", instructor: instructor
+      g12 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "202", instructor: instructor
+      g21 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "501", instructor: instructor
+      g22 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "502", instructor: instructor
+
+      expect(Evaluation.default_sorted_groups).to eq([[g11, g12], [g21, g22]])
+    end
+
+    it "groups instructors together" do
+      instructor2 = Instructor.create(name: "Kevin Sumlin")
+      g11 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "501", instructor: instructor2
+      g12 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "502", instructor: instructor2
+      g21 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "503", instructor: instructor
+      g22 = Evaluation.create term: "2015C", subject: "CSCE", course: "110", section: "504", instructor: instructor
+
+      expect(Evaluation.default_sorted_groups).to eq([[g11, g12], [g21, g22]])
     end
   end
 end
