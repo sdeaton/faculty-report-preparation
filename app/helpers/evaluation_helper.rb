7d4f98e1828fef6e806df0a28d47cafad4977a33
Brent Walther <brentwalther@gmail.com>
2016-04-01 20:28:10 -0500

    Compute sums and averages for groups.

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -1,2 +1,9 @@
 module EvaluationHelper
+  def compute_total_enrollment(group)
+    group.inject(0) { |sum, eval| sum += eval.enrollment }
+  end
+
+  def compute_weighted_average_for_item(x, group)
+    group.inject(0) { |sum, eval| sum += eval.enrollment * eval.send("item#{x}_mean".to_sym) } / compute_total_enrollment(group)
+  end
 end
