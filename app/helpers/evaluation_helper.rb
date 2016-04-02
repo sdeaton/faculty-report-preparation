82787602ec656ae483fc3f11d91fec0a4a040857
Brent Walther <brentwalther@gmail.com>
2016-04-01 20:34:41 -0500

    Add mean student eval score.

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -6,4 +6,8 @@ module EvaluationHelper
   def compute_weighted_average_for_item(x, group)
     group.inject(0) { |sum, eval| sum += eval.enrollment * eval.send("item#{x}_mean".to_sym) } / compute_total_enrollment(group)
   end
+
+  def compute_mean_student_eval_score(group)
+    (1..8).inject(0) { |sum, x| sum += compute_weighted_average_for_item(x, group) } / 8
+  end
 end
